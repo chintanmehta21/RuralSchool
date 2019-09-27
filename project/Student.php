e6a55a930ac4df9f31b91bc21c28be64bbf92fc0
chinthub <34465905+chinthub@users.noreply.github.com>
2019-09-27 12:20:14 +0530

    Add files via upload

---
+++ project/Student.php
@@ -0,0 +1,28 @@
+<?php
+require "conn.php";
+$name = $_POST["fullname"];
+$email = $_POST["email"];
+$username = $_POST["username"];
+$password = $_POST["password"];
+$class = $_POST["classs"];
+$gender = $_POST["gender"];
+$sql = "SELECT * FROM Student WHERE E_Mail = '".$email."'";
+$sql1 = "SELECT * FROM Student WHERE Username = '".$username."'";
+$result = mysqli_query($conn,$sql);
+$result1=mysqli_query($conn,$sql1);
+if(mysqli_num_rows($result)>=1)
+{
+echo "E-Mail already exists";
+}
+elseif(mysqli_num_rows($result1)>=1){
+echo"Username already exists";
+}
+else{
+$mysql_qry = "INSERT INTO Student (Name,Email,Username,Password,Class,Gender) VALUES ('$name','$email','$username','$password','$class','$gender');";
+$result = mysqli_query($conn ,$mysql_qry);
+ if($result==1)
+ {
+ header("Location:Login.html");
+ }
+}
+?>
